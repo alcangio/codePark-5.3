e245ad34ea0826507c45b368f464816203f06a67
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-04-05 11:10:58 -0300

    teste

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let elementoH1 = document.createElement("h1");
 elementoH1.innerText = "Smartphones: lançamentos";
 elementoH1.id = "titulo";
 
-let elementoMain = document.querySelector("main");
-elementoMain.appendChild(elementoH1);
+let elementoBody = document.querySelector("body");
+elementoBody.appendChild(elementoH1);
 
 // MÉTODO COMPLEXO
 // TODO 2 : adicione um elemento que represente um produto à venda
@@ -17,4 +17,4 @@ produtoVenda.innerHTML = `
 <p>Preço: <strong>R$ 8.499,00</strong> no PIX</p>
 <img src="https://www.iplace.com.br/file/general/iplace-modelos-iphone-15-pro-max-titanio-natural-min.png" alt="imagem de um smartphone na tonalidade cinza médio com slogan da Apple" width="300">
 `
-elementoMain.appendChild(produtoVenda);
+elementoBody.appendChild(produtoVenda);
